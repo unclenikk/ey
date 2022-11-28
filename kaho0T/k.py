0f495440deb2c10383f67e39ccdd724689669779
unclenikk <nikaamrazishvili@gmail.com>
2022-11-28 18:53:39 +0400

    Type commit

--- kaho0T/k.py
+++ kaho0T/k.py
@@ -686,7 +686,7 @@ print(friends["dato"][0])
 
 def twice_as_old(dad_years_old, son_years_old):
     return abs(dad_years_old - 2 * son_years_old)
-print(twice_as_old(47,14)*(33,9))
+print(twice_as_old(47,14)*twice_as_old(33,9))
 #285
 
 
